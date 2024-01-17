1bc7f4e7feb109bb88f60d0365c01a502cc03742
FarahBelhajammar <farah.belhajammar@insat.ucar.tn>
2024-01-17 22:21:41 +0100

    Revert "add light mode/boostrap don't working"
    This reverts commit 64381365f67662500ba5bb49f1c482e70a6cb53b.

--- Expense Tracker/wwwroot/js/site.js
+++ Expense Tracker/wwwroot/js/site.js
@@ -1,40 +1,4 @@
-﻿
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
 
-const themeToggleBtn = document.getElementById('theme-switcher');
-const themeName = 'dark-theme';
-const darkTheme = localStorage.getItem(themeName);
-
-// Vérification du thème actuel
-if (darkTheme === null) {
-    localStorage.setItem(themeName, 'inactive');
-} else if (darkTheme === 'active') {
-    toggleTheme();
-}
-
-// Écouteur d'événement pour le changement de thème
-themeToggleBtn.addEventListener('click', function () {
-    toggleTheme();
-});
-
-// Fonction pour basculer entre les thèmes
-function toggleTheme() {
-
-    const isDarkTheme = document.body.classList.contains('dark-theme');
-    const allElements = document.querySelectorAll('*'); // Sélectionnez tous les éléments du site
-
-    if (isDarkTheme) {
-        // Si c'est en mode sombre, basculer vers le mode clair pour chaque élément
-        allElements.forEach(element => {
-            element.classList.remove('dark-theme');
-            // Autres modifications de style pour le mode clair si nécessaire
-        });
-        localStorage.setItem(themeName, 'inactive');
-    } else {
-        // Sinon, basculer vers le mode sombre pour chaque élément
-        allElements.forEach(element => {
-            element.classList.add('dark-theme');
-            // Autres modifications de style pour le mode sombre si nécessaire
-        });
-        localStorage.setItem(themeName, 'active');
-    }
-}
+// Write your JavaScript code.
