019d2b254ca101bef5c5cb73a37a5b5df64bc08b
Saber Azzouzi <Saber.Azzouzi@expensya.com>
2024-02-06 22:49:05 +0100

    commit bug

---
+++ Expense Tracker/Models/Mission.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Expense_Tracker.Models
+{
+    public class Mission
+    {
+        [Key]
+        public int MissionId { get; set; }
+
+        public int UserId { get; set; }
+        public ApplicationUser Owner { get; set; }
+        public ICollection<Transaction> Transactions { get; set; }
+
+        public DateTime Date { get; set; } = DateTime.Now;
+
+    }
+}
